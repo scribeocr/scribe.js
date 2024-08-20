7c62e245a906a3f8c4c92ee9c47fbbd902ae33bc
Balearica <admin@scribeocr.com>
2024-08-19 19:20:02 -0700

    Updated import

--- js/worker/generalWorker.js
+++ js/worker/generalWorker.js
@@ -17,7 +17,7 @@ import { optimizeFont } from './optimizeFontModule.js';
 // import Tesseract from "../../tess/tesseract.esm.min.js";
 const browserMode = typeof process === 'undefined';
 
-const Tesseract = browserMode ? (await import('../../tess/tesseract.esm.min.js')).default : await import('tesseract.js/src/index.js');
+const Tesseract = browserMode ? (await import('../../tess/tesseract.esm.min.js')).default : await import('@scribe.js/tesseract.js');
 
 const defaultConfigs = {
   // TODO: Add back support for multiple PSM modes.
